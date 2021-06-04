349c09630a9f7986729b00b6161741e74fd38fdf
Jerud-Moyer <jerudmoyer@gmail.com>
2021-06-04 11:08:43 -0700

    demo complete

--- src/components/characters/CharacterItem.tsx
+++ src/components/characters/CharacterItem.tsx
@@ -1,1 +1,36 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import styles from './CharacterItem.module.css'
 
+interface Props {
+  id: number;
+  name: string;
+  imageUrl: string;
+}
+
+const CharacterItem: React.FC<Props> = ({
+id,
+name,
+imageUrl
+} 
+) => {
+  return (
+    <div>
+      <Link
+        to={`/detail/${id}`}
+        className={styles.link}
+      >
+        <figure>
+    <img
+      src={imageUrl}
+      alt={name}
+      className={styles.img}
+    />
+    <figcaption>{name}</figcaption>
+        </figure>
+      </Link>
+    </div>
+  )
+}
+
+export default CharacterItem
